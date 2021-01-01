3ebfe018018a7a5f76b92a22c798aa0c61abb73b
Keeley <kstmoore@googlemail.com>
2021-01-01 18:00:20 +0000

    More poking around

--- src/scenes/fractals/kotch/Kotch.tsx
+++ src/scenes/fractals/kotch/Kotch.tsx
@@ -1,5 +1,5 @@
 import React, { FC, useLayoutEffect, useRef } from 'react';
-import { Canvas, useThree, useUpdate } from 'react-three-fiber';
+import { Canvas, useFrame, useThree, useUpdate } from 'react-three-fiber';
 import * as THREE from 'three';
 import { Camera } from '../../../components/DefaultCamera';
 import { useControlsContext } from '../../../controls';
@@ -12,26 +12,29 @@ interface LinesProps {
 
 const InstancedLine: FC<LinesProps> = ({ x1, x2, y1, y2 }) => {
   const ref = useRef<THREE.InstancedMesh>();
-  // const lineBufferRef = useUpdate<THREE.BufferGeometry>(geometry => {
-  //   geometry.setFromPoints([new THREE.Vector2(x1, y1), new THREE.Vector2(x2, y2)]);
-  // }, []);
 
-  useLayoutEffect(() => {
-    const transform = new THREE.Matrix4();
-    console.log(new THREE.Vector3(x1, y1));
-    transform.setPosition(new THREE.Vector3(x1, y1), x2, 0);
-    // for (let i = 0; i < 10000; ++i) {
-    //   const x = (i % 100) - 50;
-    //   const y = Math.floor(i / 100) - 50;
-    //   transform.setPosition(x, y, 0);
-    //   ref.current!.setMatrixAt(i, transform);
-    // }
-  }, [x1, y1, x2]);
+  const vec = new THREE.Vector3();
+  const transform = new THREE.Matrix4();
+  const position = new THREE.Vector3();
+
+  position.x = -0.02574881765633208;
+  position.y = -1;
+  console.log(position);
+
+  useFrame(({ clock }) => {
+    vec.copy(position).multiplyScalar(Math.sin(4) + 1.3);
+
+    // Apply the Vector3 to a Matrix4
+    transform.setPosition(position);
+    // Update Matrix4 for this instance
+    ref.current!.setMatrixAt(1, transform);
+
+    ref.current!.instanceMatrix.needsUpdate = true;
+  });
 
   return (
     <instancedMesh ref={ref} args={[null as unknown as THREE.BufferGeometry, null as unknown as THREE.Material, 1]}>
       <bufferGeometry />
-      {/* <circleBufferGeometry args={[0.15]} /> */}
       <lineBasicMaterial color={0x0000ff} vertexColors />
     </instancedMesh>
   );
@@ -70,13 +73,13 @@ const CurveOrLine: FC<CurveProps> = ({ x1, x2, y1, y2, currentDept, depth, alpha
 const Curve: FC<CurveProps> = ({ x1, x2, y1, y2, currentDept, depth, alpha }) => {
   const xThird = (x2 - x1) / 3,
     yThird = (y2 - y1) / 3;
-
   const xa = x1 + xThird,
     ya = y1 + yThird,
     xc = x2 - xThird,
     yc = y2 - yThird,
     xb = xa + (xc - xa) * Math.cos(-alpha) - (yc - ya) * Math.sin(-alpha),
     yb = ya + (yc - ya) * Math.cos(-alpha) + (xc - xa) * Math.sin(-alpha);
+
   return (
     <>
       <CurveOrLine key={x1} x1={x1} y1={y1} x2={xa} y2={ya} alpha={alpha} depth={depth} currentDept={currentDept} />

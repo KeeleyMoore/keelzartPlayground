d3a3fad9bf54063992ea1e77bb25499cd2241439
Keeley <kstmoore@googlemail.com>
2021-01-16 15:58:05 +0000

    Draw simple cantor re-written

--- src/scenes/fractals/cantors/Cantor.tsx
+++ src/scenes/fractals/cantors/Cantor.tsx
@@ -1,87 +1,43 @@
 import React, { FC, useMemo } from 'react';
-import { Canvas } from 'react-three-fiber';
+import { Canvas, useThree } from 'react-three-fiber';
 import * as THREE from 'three';
 
 import { DefaultCamera } from '../../../components';
 
-const DrawCantor: FC = () => {
-  const division = 0.33,
-    divisionCap = 35,
-    strokeWidth = 1,
-    startLength = 10,
-    startLengthCap = 100,
-    rotate = true,
-    height = window.innerHeight - 120,
-    width = window.innerWidth,
-    verticalStart = true;
+const DrawSimpleCantor: FC = () => {
+  const { viewport } = useThree();
+  const startLength = 80,
+    width = viewport.width;
 
+  const length = (width / 100) * startLength;
+  const division = 1 / 3;
 
-  const xStart = (width / 100) * startLength,
-    yStart = height / 2,
-    len = width - (xStart * 2);
+  const xStart = 0 - length / 2,
+    yStart = 0;
+
   const { points } = useMemo(() => {
-    console.log('x and y', xStart, yStart);
-    let points = [xStart, yStart, 0, 0 + startLength / 2, 0, 0];
+    const generateCantorPoints = (x: number, y: number, length: number) => {
+      points.push(x, y, 0, x + length, y, 0);
 
-    // verticalStart ? [xStart, yStart - len / 2, 0, xStart, yStart + len / 2, 0];
-    const addVertical = (x: number, y: number, length: number) => {
-      const x1 = x - length * division;
-      const x2 = x + length * division;
-      const y1 = y - length / 2;
-      const y2 = y + length / 2;
-
-      points.push(x, y, 0, x1, y1, 0, x2, y2, 0);
-      console.log('pushed', x1, y1, 0, x2, y2, 0);
-      const len = length * (division * 2);
-
-      generateCantorPoints(x1, y1, len, false);
-      generateCantorPoints(x2, y2, len, false);
-    };
-
-    const addHorizontal = (x: number, y: number, length: number) => {
-      const x1 = x;
-      const x2 = x + length;
-      const y1 = y;
-      const y2 = y;
-      const len = length * (division * 2);
-      points.push(x, y, 0, x1, y1, 0, x2, y2, 0);
-
-      console.log('pushed', x1, y1, 0, x2, y2, 0);
-      generateCantorPoints(x1, y1, len, true);
-      generateCantorPoints(x2, y2, len, true);
-    };
-    let counter = 0;
-
-    const generateCantorPoints = (x: number, y: number, length: number, vertical: boolean) => {
-      // console.log('generate', x, vertical, length);
-      counter++;
-      if (counter < 5) {
-        console.log(counter, 'counter');
-        if (Math.abs(length) < 270) {
-          if (vertical) {
-            console.log('vertical', x);
-            return addVertical(x, y, length);
-          }
-          addHorizontal(x, y, length);
-        }
+      if (length > 1) {
+        const newY = y + 10;
+        generateCantorPoints(x, newY, length * division);
+        generateCantorPoints(x + (length * (division * 2)), newY, length / 3);
       }
     };
-    generateCantorPoints(xStart, yStart, startLength, false);
-    // generateCantorPoints(0 - startLength, 0, startLength, false);
+
+    let points: number[] = [];
+    generateCantorPoints(xStart, yStart, length);
 
     return { points };
 
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  // console.log(points);
+  }, [length, xStart, division]);
 
   return (
     <lineSegments>
       <bufferGeometry
         attach="geometry"
         onUpdate={geometry => {
-          // geometry.setIndex(indices);
           geometry.setAttribute('position', new THREE.Float32BufferAttribute(points, 3));
         }}
       />
@@ -94,9 +50,9 @@ const Cantor: FC = () => {
 
   return (
     <Canvas>
-      <DefaultCamera zoom={10} position={[0, 0, 5]} />
+      <DefaultCamera zoom={1} position={[0, 0, 5]} />
       <color attach="background" args={[0, 0, 0]} />
-      <DrawCantor />
+      <DrawSimpleCantor />
     </Canvas>
   );
 };
